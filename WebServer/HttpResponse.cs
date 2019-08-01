65993abad70f101159b7bc50376c8713906fea48
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-08-01 10:46:54 +0530

    Added default path as index.html

--- WebServer/HttpResponse.cs
+++ WebServer/HttpResponse.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 
 namespace CsharpListenerDemo
 {
     public class HttpResponse
     {
+        private string _extractedFileName;
+        private HttpListenerResponse _response;
+        private string _filePath;
         public void WriteResponse(HttpRequest httpRequest)
         {
-            var extractedFileName = httpRequest.GetFileNameFromUrl();
-            var response = httpRequest.GetContext().Response;
+            _extractedFileName = httpRequest.GetFileNameFromUrl();
+            _response = httpRequest.GetContext().Response;
             try
             {
-                FileStream fileStream = new FileStream(extractedFileName + ".html", FileMode.Open, FileAccess.Read);
-                var buffer = File.ReadAllBytes(extractedFileName + ".html");
-                fileStream.Read(buffer, 0, Convert.ToInt32(fileStream.Length));
-                response.ContentLength64 = buffer.Length;
-
-                var output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                if (string.IsNullOrEmpty(_extractedFileName))
+                {
+                    _filePath = "WebPages/index.html";
+                }
+                else
+                {
+                    _filePath = "WebPages/" + _extractedFileName + ".html";
+                }
+                RenderFileToBrowser();
             }
             catch(FileNotFoundException e)
             {
-                FileStream fileStream = new FileStream("NotFound.html", FileMode.Open, FileAccess.Read);
-                var buffer = File.ReadAllBytes("NotFound.html");
-                fileStream.Read(buffer, 0, Convert.ToInt32(fileStream.Length));
-                response.ContentLength64 = buffer.Length;
-
-                var output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                _filePath = "WebPages/NotFound.html";
+                RenderFileToBrowser();
             }
+        }
+        private void RenderFileToBrowser()
+        {
+            FileStream fileStream = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
+            var buffer = File.ReadAllBytes(_filePath);
+            fileStream.Read(buffer, 0, Convert.ToInt32(fileStream.Length));
+            _response.ContentLength64 = buffer.Length;
+            var output = _response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
         }
     }
 }
